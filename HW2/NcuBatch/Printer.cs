01e593fed2b61373e209446524045014d7e64b29
Diman Todorov <diman.todorov@outlook.com>
2021-02-09 23:53:47 -0800

    batch generator

---
+++ HW2/NcuBatch/Printer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NcuBatch
+{
+    class Printer
+    {
+        private IEnumerable<string> kernels = new List<string>() { "matAdd", "matAddRow", "matAddCol", "dgemv" };
+        private IEnumerable<Tuple<int, int>> sizes = new List<Tuple<int, int>>
+        {
+            new Tuple<int, int>(4, 32),
+            new Tuple<int, int>(128, 32),
+            new Tuple<int, int>(1024, 32),
+        };
+
+        private string ncuTemplate = @"ncu -k {0} --metrics smsp__sass_thread_inst_executed_op_fadd_pred_on.sum,smsp__sass_thread_inst_executed_op_ffma_pred_on.sum,smsp__sass_thread_inst_executed_op_fmul_pred_on.sum,smsp__sass_thread_inst_executed_op_dadd_pred_on.sum,smsp__sass_thread_inst_executed_op_dfma_pred_on.sum,smsp__sass_thread_inst_executed_op_dmul_pred_on.sum,dram__bytes.sum,dram__bytes_read.sum,dram__bytes_write.sum,l1tex__t_bytes.sum,lts__t_bytes.sum,sm__cycles_elapeed.avg,sm__cycles_elapsed.avg.per_second,smsp__sass_thread_inst_executed_op_integer_pred_on.sum,sm__inst_executed_pipe_alu  --set default CudaRun.exe ";
+ 
+        public int ipow2(int e)
+        {
+            return 1 << e;
+        }
+        
+        public void Print()
+        {
+            foreach (var kernel in kernels)
+            {
+                foreach (var size in sizes)
+                {
+                    for (int e = 8; e <= 26; e += 2)
+                    {
+                        var param = $"-k {kernel} -b {size.Item1} -t {size.Item2} -r {ipow2(e/2)}";
+                        var output = $" > {kernel}.txt";
+                        Console.WriteLine(string.Format(ncuTemplate, kernel) + param);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ HW2/NcuBatch/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NcuBatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var printer = new Printer();
+            printer.Print();
+        }
+    }
+}
