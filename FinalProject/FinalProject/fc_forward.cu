27813a900a23ee9364fb1331f9a5a1a6ead24fda
Diman Todorov <ditodoro@microsoft.com>
2021-03-18 14:17:34 -0700

    output layer

--- FinalProject/FinalProject/fc_forward.cu
+++ FinalProject/FinalProject/fc_forward.cu
@@ -10,6 +10,14 @@ __device__ __host__ __forceinline__ float relu(float f)
     return (fmaxf(0, f));
 }
 
+__device__ __host__ __forceinline__ float reluPrime(float f)
+{
+    if (0.f == f)
+        return 0.5f;
+    else
+        return (f < 0.f ? 0.0f : 1.0f);
+}
+
 __device__ __host__ __forceinline__ float softmax10(float* fvec10, int ti)
 {
     float sum = 0;
@@ -23,6 +31,16 @@ __device__ __host__ __forceinline__ float softmax10(float* fvec10, int ti)
     }
     return (target / sum);
 }
+
+// fvec10: activations from previous layer
+// ds_i: perceptron index within the layer
+// ds_k: index of vector element towards which we are taking a partial derivative
+__device__ __host__ __forceinline__ float softmax10Prime(float* fvec10, int ds_i)
+{
+    float si = softmax10(fvec10, ds_i);
+    return si * (1 - si);
+}
+
 
 __global__ void fc_forward(const float* X, const float* W, const float* b, float* Z, float* Y, const int n, const int activation)
 {
@@ -49,7 +67,15 @@ __global__ void fc_forward(const float* X, const float* W, const float* b, float
     }
 }
 
+__global__ void output_backward(float* labels, float* Yhat, float* Z, int n, float* dY)
+{
+    int ds_i = threadIdx.x;
+
+    dY[ds_i] = (labels[ds_i] - Yhat[ds_i]) * softmax10Prime(Z, ds_i);
+}
+
 // last layer: deltaL = (y hat - y) activation function'(previous layer activation)
 // any other layer: 
 // https://machinelearningmastery.com/implement-backpropagation-algorithm-scratch-python/
 // https://cloudacademy.com/course/data-machine-learning-gradient-descent/derivative-calculation/
+// https://medium.com/@14prakash/back-propagation-is-very-simple-who-made-it-complicated-97b794c97e5c
